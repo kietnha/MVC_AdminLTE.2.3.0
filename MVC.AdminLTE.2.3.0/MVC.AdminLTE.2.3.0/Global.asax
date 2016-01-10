83c2ce5cbdfb627b683099bff04404af856dcf2a
Nguyễn Hoàng Anh Kiệt <kietnha@gmail.com>
2016-01-10 20:38:08 +0700

    1/ sử dụng layout LTE2.3 OK

---
+++ MVC.AdminLTE.2.3.0/MVC.AdminLTE.2.3.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC.AdminLTE._2._3._0.MvcApplication" Language="C#" %>
---
+++ MVC.AdminLTE.2.3.0/MVC.AdminLTE.2.3.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC.AdminLTE._2._3._0.Startup))]
+namespace MVC.AdminLTE._2._3._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
